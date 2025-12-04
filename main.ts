6e956f5e317f4bd6797ca588d3fa902796ae8d3e
tipyman <149936594+tipyman@users.noreply.github.com>
2025-12-04 11:26:32 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 /**
- * makecode ZETA-R3 module Package Release 1
- * By 2025 Socionext Inc. and ZETA alliance Japan
- * Written by M.Urade　2025/11/28
- */
+* makecode ZETA-R3 module Package Release 1
+* By 2025 Socionext Inc. and ZETA alliance Japan
+* Written by M.Urade　2025/12/4
+*/
 
 /**
  * ZETA-R3 block Ver1
  */
-
 //% weight=100 color=#0096FF icon="\uf434" block="ZETA-R3"
+
 namespace ZETA_R3 {
     let buffer: Buffer = Buffer.create(0)
     let dataBuffer = pins.createBuffer(1);
@@ -72,7 +72,7 @@ namespace ZETA_R3 {
         TX_array.push((crc16_data >> 8) & 0xff)
         TX_array.push(crc16_data & 0xff)
         let k = 0
-        for (let i = 0; i < Array_length + 2; i++) {
+        for (let l = 0; l < Array_length + 2; l++) {
             UART_BIN_TX(TX_array[k])
             k += 1
         }
@@ -91,15 +91,15 @@ namespace ZETA_R3 {
     //% blockId=ZETA_data_transmission block="ZETA data transmission %TX_array"
     //% weight=80 blockGap=8
     export function data_tx(TX_array: number[]) {
-        let Array_length = TX_array.length
-        let data_array = [0xfa, 0xf5, Array_length + 3, 2]
-        let Query_array = command_assert(data_array.concat(TX_array))
-        return Query_array[3]
+        let Array_length2 = TX_array.length
+        let data_array = [0xfa, 0xf5, Array_length2 + 3, 2]
+        let Query_array2 = command_assert(data_array.concat(TX_array))
+        return Query_array2[3]
     }
 
     //% blockId=Inquiry MAC address block="Inquiry MAC address"
     //% weight=80 blockGap=8
-    export function Inquire_MAC() {
+    export function Inquire_MAC(): number []{
         let temp = [0, 0, 0, 0, 0, 0, 0, 0]
         temp = command_assert([0xfa, 0xf5, 0x03, 0x10])
         temp[0] = temp[4]
@@ -111,37 +111,58 @@ namespace ZETA_R3 {
 
     //% blockId= Inquire_Module_Status block="Inquire Module Status"
     //% weight=80 blockGap=8
-    export function Inquire_Module_Status() {
+    export function Inquire_Module_Status(): number {
         let temp = command_assert([0xfa, 0xf5, 0x03, 0x14])
         return temp[3]
     }
 
+    //% blockId= Inquire_Version="Inquire Protocol version"
+    //% weight=80 blockGap=8
+    export function Inquire_Version():number {
+        let temp = command_assert([0xfa, 0xf5, 0x03, 0])
+        return ((temp[4] << 8 ) + temp[5]);
+    }
+
+    //% blockId= Inquire_Network_Time="Inquire network time"
+    //% weight=80 blockGap=8
+    export function Inquire_Network_Time() :number []{
+        const temp = command_assert([0xfa, 0xf5, 0x03, 0x11]).slice(4, 11);
+        return temp;
+    }
+
+    //% blockId= Inquire_Network_Quality="Inquire network quality"
+    //% weight=80 blockGap=8
+    export function Inquire_Network_Quality():number {
+        const temp = command_assert([0xfa, 0xf5, 0x03, 0x13]);
+        return temp[4];
+    }
+
     //% blockId= Receive_query data block="Receive query data"
     //% weight=80 blockGap=8
-    export function receive_query() {
-        let temp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        let k = 0   // Waiting timer
+    export function receive_query():number [] {
+        let temp3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        let m = 0   // Waiting timer
         while(1){
             let data = UART_BIN_RX()
             if (data == 0xfa){
                 break;
             }
-            k += 1;
-            if (k > 15){
-                return temp
+            m += 1;
+            if (m > 15){
+                return temp3
             }
         }
         if (UART_BIN_RX() == 0xf5) {
-            temp[0] = 0xfa
-            temp[1] = 0xf5
+            temp3[0] = 0xfa
+            temp3[1] = 0xf5
             let counter = UART_BIN_RX()
-            temp[2] = counter
-            k = 3
-            for (let i = 0; i < counter; i++) {
-                temp[k] = UART_BIN_RX()
-                k += 1
+            temp3[2] = counter
+            m = 3
+            for (let n = 0; n < counter; n++) {
+                temp3[m] = UART_BIN_RX()
+                m += 1
             }
         }
-        return temp
+        return temp3
     }
 }
